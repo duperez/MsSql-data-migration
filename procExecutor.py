553ed761aa2641057ca40d90951a754c40193fc9
duperez <eduardobianconiperez@gmail.com>
2020-02-10 13:15:58 -0300

    classe criada para auxiliar a separacao de dados

--- procExecutor.py
+++ procExecutor.py
@@ -1,60 +1,71 @@
-import pymssql
+import re
 import sys
 from os import listdir
-from os.path import isfile, join
+import pymssql
+import operator
 
-#           _      _      _        
-#  ___ __ _| |  __| |__ _| |_ __ _ 
+#           _      _      _
+#  ___ __ _| |  __| |__ _| |_ __ _
 # (_-</ _` | | / _` / _` |  _/ _` |
 # /__/\__, |_| \__,_\__,_|\__\__,_|
-#        |_|                       
+#        |_|
 
-server = "localhost"
-user = "SA"
+server   = "localhost"
+user     = "SA"
 password = "your@pass123"
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
+class sql_file:
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+        self.data = ''
+
+    def set_data(self):
+        sql = open(self.path + self.name, 'r')
+        proc = ''
+        for line in sql:
+            proc = proc + line
+            self.data = proc
+
+
+class color:
+    HEADER    = '\033[95m'
+    OKBLUE    = '\033[94m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAIL      = '\033[91m'
+    ENDC      = '\033[0m'
     UNDERLINE = '\033[4m'
-
-
-def get_database_info():
-    lst = list()
-    info = open('data.txt', 'r')
-    for line in info:
-        lst.append(line)
-    return lst
-
-
-def read_proc_file(name):
-    file = open(name, 'r')
-    proc = ''
-    for line in file:
-        proc = proc + line
-    return proc
 
 
 def execute(connection, proc):
     connection.autocommit(True)
     cursor = connection.cursor()
-    cursor.execute(proc)
+    cursor.execute(proc.data)
+
+
+def get_all_files(dir):
+    all_file = list()
+    folder = [f for f in listdir(dir)]
+    for file in folder:
+        if re.match('.*sql', file):
+            all_file.append(sql_file(dir, file))
+        else:
+            if file != dir:
+                all_file = all_file + get_all_files(dir + file + '/')
+    return all_file
 
 
 def set_environment(folder, connection):
-    print(bcolors.HEADER + "setting environment up" + bcolors.HEADER)
-    files = [f for f in listdir(folder) if isfile(join(folder, f))]
-    files.sort()
+    print(color.HEADER + "setting environment up" + color.HEADER)
+    files = get_all_files(folder)
+    files.sort(key=lambda x: x.name, reverse=False)
     for proc in files:
-        execute(connection, read_proc_file(folder + proc))
-        print(bcolors.WARNING + proc + bcolors.OKBLUE + " is now up to date")
-    print(bcolors.OKGREEN + "environment configured" + bcolors.OKGREEN)
+        proc.set_data()
+        execute(connection, proc)
+        print(color.WARNING + proc.name + color.OKBLUE + " is now up to date")
+    print(color.OKGREEN + "environment configured" + color.OKGREEN)
 
 
 try:
@@ -62,8 +73,8 @@ try:
     set_environment(sys.argv[1], conn)
     conn.close()
 
-    print(bcolors.OKGREEN + bcolors.UNDERLINE + "process finished \\o/ - the end\n" + bcolors.ENDC)
+    print(color.OKGREEN + color.UNDERLINE + "process finished \\o/ - the end\n" + color.ENDC)
 except Exception as ex:
     template = "An exception of type {0} occurred. Arguments:\n{1!r}"
     message = template.format(type(ex).__name__, ex.args)
-    print(bcolors.FAIL + message)
+    print(color.FAIL + message)
